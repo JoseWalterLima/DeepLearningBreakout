0d0f820c422b7695a98a3993bbc93edf5a4f7e5a
JoseWalterLima <jwl.mota@gmail.com>
2022-07-20 15:46:53 -0300

    'adjust file name'

--- visionapi.py
+++ visionapi.py
@@ -8,7 +8,6 @@ top = y/2 - 370
 right = x/2 + 470
 bottom = y/2 + 370
 ss_region = (left, top, right, bottom)
-#time.sleep(4)
 ss_img = ImageGrab.grab(ss_region).convert("L")
 
 ss_img.save("SS3.jpg")
