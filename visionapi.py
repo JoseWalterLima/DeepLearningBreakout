f5e4bdf7b559a7f6a44e309b3ea22edd49073c3d
JoseWalterLima <jwl.mota@gmail.com>
2022-07-21 12:10:15 -0300

    convertion of screenshot to pytorch tensor

--- visionapi.py
+++ visionapi.py
@@ -1,6 +1,8 @@
 from PIL import ImageGrab
 import pyautogui
 import time
+import torch
+import torchvision.transforms as transforms
 
 x, y = pyautogui.size()
 left = x/2 - 500
@@ -8,7 +10,13 @@ top = y/2 - 370
 right = x/2 + 470
 bottom = y/2 + 370
 ss_region = (left, top, right, bottom)
-time.sleep(5)
+t1 = time.time()
 ss_img = ImageGrab.grab(ss_region).convert("L").resize((200,200))
+
+# Transforma image to pytorch tensor
+transform = transforms.Compose([transforms.PILToTensor()])
+ss_img = transform(ss_img)
+t2 = time.time()
 print(type(ss_img))
+print('Total Time: {}'.format((t2 - t1)))
 #ss_img.save(str(time.time()) + '.png')
